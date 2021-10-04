e7cdce7ff33edce62746a88ebeccd25bbbe39c6d
Juanita Blignaut <91057045+JuanitaBlignaut@users.noreply.github.com>
2021-10-04 12:14:45 +0200

    Update Form1.cs
    Reaing text file

--- QuizPrg282/Form1.cs
+++ QuizPrg282/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace QuizPrg282
 {
@@ -19,7 +20,18 @@ namespace QuizPrg282
 
         private void btnRead_Click(object sender, EventArgs e)
         {
-
+            string path = @"C:\Users\Juani\Desktop\2nd_Year\QuizPrg282\QuizPrg282\bin\Debug\Users.txt";
+            if (File.Exists(path))
+            {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string txt;
+                while((txt = reader.ReadLine())!= null)
+                {
+                    listBox1.Items.Add(txt);
+                }
+            }
+            }
         }
     }
 }
